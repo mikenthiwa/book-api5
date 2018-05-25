5b6f8ea195e0a7181622f605f8ba909b7364cf41
Michael Nthiwa <mike.nthiwa@gmail.com>
2018-05-25 21:05:11 +0300

    fix app.py

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ config_name = os.getenv("APP_SETTINGS")
 app = create_app(config_name)
 
 if __name__ == '__main__':
-    app.run()
-    # port = int(os.environ.get("PORT", 5000))
-    # app.run('0.0.0.0', port=port)
+    # app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run('0.0.0.0', port=port)
