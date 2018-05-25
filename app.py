a02f7a934f0d7b2c1b7c0b6bcee096068d5ee381
Michael Nthiwa <mike.nthiwa@gmail.com>
2018-05-25 20:50:54 +0300

    Fix Port

--- app.py
+++ app.py
@@ -7,5 +7,6 @@ app = create_app(config_name)
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run('0.0.0.0', port=port)
+    app.run()
+    # port = int(os.environ.get("PORT", 5000))
+    # app.run('0.0.0.0', port=port)
