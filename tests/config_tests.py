15dbdc16690aff602083edcdaeefbec9b73b3b3b
Michael Nthiwa <mike.nthiwa@gmail.com>
2018-05-25 12:07:36 +0300

    Setting base configuration for tests

--- tests/config_tests.py
+++ tests/config_tests.py
@@ -1,1 +1,30 @@
+import unittest
+import sys  # fix import errors
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+from app import db, create_app
+
+
+class ConfigTestCase(unittest.TestCase):
+    """This class represents the books test case"""
+
+    def setUp(self):
+        """Define test variables and initialize app"""
+        self.app = create_app(config_name="testing")
+        self.client = self.app.test_client
+        self.books = {"title": "The storm", "author": "Blake Banner", "copies": 8}
+        self.title = self.books["title"]
+        self.author = self.books["author"]
+        self.copies = self.books["copies"]
+        # binds the app to the current context
+        with self.app.app_context():
+            # create all tables
+            db.create_all()
+
+    def tearDown(self):
+        """teardown all initialized variables."""
+        with self.app.app_context():
+            # drop all tables
+            db.session.remove()
+            db.drop_all()
