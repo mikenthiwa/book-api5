441c7da86d496033c0932f1ee7ec08b93b4a602e
Michael Nthiwa <mike.nthiwa@gmail.com>
2018-05-27 16:45:19 +0300

    Changing db name

--- .travis.sh
+++ .travis.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 export SECRET_KEY=supersecret
-export TESTING_DATABASE_URI=postgresql://postgres@localhost/test_book_api5
+export TESTING_DATABASE_URI=postgresql://postgres@localhost/travis_tests
