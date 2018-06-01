916544d5d4e67ffb138614a7885e1479c3586703
Michael Nthiwa <mike.nthiwa@gmail.com>
2018-06-01 08:14:57 +0300

    Fix pep8 issues

--- resources/books.py
+++ resources/books.py
@@ -4,22 +4,22 @@ from app.models import Books
 api = Namespace('Books', description='Books related operations')
 
 
-
-
 class BookLists(Resource):
-
-    def get(self):
+    """class contains GET method"""
+    @staticmethod
+    def get():
         """Get all books"""
         response = Books.get_all_books()
         return response
 
 
 class Book(Resource):
-    def get(self, book_id):
+    """class contains GET method"""
+    @staticmethod
+    def get(book_id):
         """Get one book"""
         response = Books.get_a_book(book_id=book_id)
         return response
-
 
 
 api.add_resource(BookLists, '/books',
